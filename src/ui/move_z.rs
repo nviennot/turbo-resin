ffd2755c389374bbbaa0cc70e7e45018b81af2ce
Joe Vanderstelt <jvanderstelt@now-connect.net>
2023-02-01 20:52:18 -0500

    New layout for move z

--- src/ui/move_z.rs
+++ src/ui/move_z.rs
@@ -3,13 +3,12 @@
 use futures::Future;
 use lvgl::{
     style::State,
-    core::Screen,
     widgets::*,
     prelude::*,
 };
 use alloc::format;
 
-use lvgl::cstr_core::CStr;
+use lvgl::cstr_core::{CStr,CString};
 use crate::{
     TaskRunner,
     drivers::zaxis::{
@@ -17,152 +16,218 @@ use crate::{
         prelude::*,
     }, util::CancellableTask,
 };
-use crate::consts::zaxis::motion_control::*;
+use lvgl::core::Display;
+use lvgl::core::Event;
+use lvgl::core::InputDevice;
+use lvgl::core::InputDeviceState;
+use lvgl::core::Lvgl;
+use lvgl::core::ObjExt;
+use lvgl::core::Screen;
+use lvgl::core::TouchPad;
+use lvgl::style::Style;
+use lvgl::{style::Align, style::Flag, style::GridAlign};
+use lvgl::style;
 
 pub struct MoveZ {
-    btn_move_up: Btn<Self>,
-    btn_move_down: Btn<Self>,
-    speed_slider: Slider<Self>,
-    speed_label: Label<Self>,
-    position_label: Label<Self>,
-    btn_move_zero: Btn<Self>,
+    style: Style,
+    col_dsc: Box<[i16; 4]>,
+    row_dsc: Box<[i16; 5]>,
+    btn_0_1mm: Btn<MoveZ>,
+    btn_1mm: Btn<MoveZ>,
+    btn_10mm: Btn<MoveZ>,
+    btn_up: Btn<MoveZ>,
+    btn_home: Btn<MoveZ>,
+    btn_down: Btn<MoveZ>,
+    current_pos: Label<MoveZ>,
 
     task_runner: &'static TaskRunner<Task>,
     zaxis: &'static zaxis::MotionControlAsync,
+
 }
+
+use alloc::boxed::Box;
 
 impl MoveZ {
     pub fn new(
-        screen: &mut Screen::<Self>,
+        screen: &mut Screen<Self>,
         task_runner: &'static mut TaskRunner<Task>,
         zaxis: &'static zaxis::MotionControlAsync,
     ) -> Self {
-        use lvgl::widgets::*;
-        use lvgl::style::*;
-        use lvgl::core::*;
+        let mut style = Style::new();
+        style.set_pad_all(10);
 
-        let spacing = 12;
+        screen.add_style(&mut style, 0);
 
-        let btn_move_up = Btn::new(screen).apply(|obj| {
-            Label::new(obj)
-                .set_text(&CStr::from_bytes_with_nul(b"Move Up\0").unwrap());
-            obj
-            .align_to(screen, Align::TopMid, 0, 2*spacing)
-            .add_flag(Flag::CHECKABLE)
-            .on_event(Event::Clicked, |context| {
-                let checked = context.btn_move_up.has_state(State::CHECKED);
-                if checked {
-                    context.task_runner.enqueue_task(Task::MoveUp).unwrap();
-                } else {
-                    context.task_runner.cancel_task();
-                }
-            });
+        let mut col_dsc = Box::new([
+            style::grid_free(1),
+            style::grid_free(1),
+            style::grid_free(1),
+            style::grid_last(),
+        ]);
+        let mut row_dsc = Box::new([
+            style::grid_free(1),
+            style::grid_free(1),
+            style::grid_free(1),
+            style::grid_free(1),
+            style::grid_last(),
+        ]);
+
+        screen.set_grid_dsc_array(col_dsc.as_mut_ptr(), row_dsc.as_mut_ptr());
+
+        let btn_0_1mm = Btn::new(screen).apply(|obj| {
+            obj.on_event(Event::Clicked, |context| {
+                //println!("Clicked on: 0.1mm");
+            })
+            .set_grid_cell(
+                GridAlign::Stretch,
+                0,
+                1,
+                GridAlign::Stretch,
+                0,
+                1,
+            );
+
+            let mut btn_lbl = Label::new(obj);
+            btn_lbl.set_text(CString::new("0.1mm").unwrap().as_c_str());
+            btn_lbl.align_to(obj, Align::Center, 0, 0);
         });
 
-        let btn_move_down = Btn::new(screen).apply(|obj| {
-            Label::new(obj)
-                .set_text(&CStr::from_bytes_with_nul(b"Move Down\0").unwrap());
-            obj
-            .align_to(&btn_move_up, Align::OutBottomMid, 0, spacing)
-            .add_flag(Flag::CHECKABLE)
-            .on_event(Event::Clicked, |context| {
-                let checked = context.btn_move_down.has_state(State::CHECKED);
-                if checked {
-                    context.task_runner.enqueue_task(Task::MoveDown).unwrap();
-                } else {
-                    context.task_runner.cancel_task();
-                }
-            });
+        let btn_1mm = Btn::new(screen).apply(|obj| {
+            obj.on_event(Event::Clicked, |context| {
+                //println!("Clicked on: 1mm");
+            })
+            .set_grid_cell(
+                GridAlign::Stretch,
+                1,
+                1,
+                GridAlign::Stretch,
+                0,
+                1,
+            );
+
+            let mut btn_lbl = Label::new(obj);
+            btn_lbl.set_text(CString::new("1mm").unwrap().as_c_str());
+            btn_lbl.align_to(obj, Align::Center, 0, 0);
         });
 
-        let speed_slider = Slider::new(screen).apply(|obj| { obj
-            .align_to(&btn_move_down, Align::OutBottomMid, 0, 2*spacing)
-            .set_range(1500, 10_000)
-            .set_value(10_000, 0)
-            .on_event(Event::ValueChanged, |context| {
-                let value = unsafe { lvgl::sys::lv_slider_get_value(context.speed_slider.raw) };
+        let btn_10mm = Btn::new(screen).apply(|obj| {
+            obj.on_event(Event::Clicked, |context| {
+                //println!("Clicked on: 10mm");
+            })
+            .set_grid_cell(
+                GridAlign::Stretch,
+                2,
+                1,
+                GridAlign::Stretch,
+                0,
+                1,
+            );
 
-                let value = (value as f32)/10000.0;
-                let value = value*value*value;
-                let value = value * MAX_SPEED;
-
-                context.zaxis.set_max_speed(value.mm());
-            });
+            let mut btn_lbl = Label::new(obj);
+            btn_lbl.set_text(CString::new("10mm").unwrap().as_c_str());
+            btn_lbl.align_to(obj, Align::Center, 0, 0);
         });
 
-        let speed_label = Label::new(screen).apply(|obj| { obj
-            .align_to(&speed_slider, Align::OutBottomLeft, 50, spacing);
+        let btn_up = Btn::new(screen).apply(|obj| {
+            obj.on_event(Event::Clicked, |context| {
+                //println!("Clicked on: up");
+            })
+            .set_grid_cell(
+                GridAlign::Stretch,
+                0,
+                1,
+                GridAlign::Stretch,
+                1,
+                1,
+            );
+
+            let mut btn_lbl = Label::new(obj);
+            btn_lbl.set_text(CString::new("UP").unwrap().as_c_str());
+            btn_lbl.align_to(obj, Align::Center, 0, 0);
         });
 
-        let position_label = Label::new(screen).apply(|obj| { obj
-            .align_to(&speed_label, Align::OutBottomLeft, 0, 0);
+        let btn_home = Btn::new(screen).apply(|obj| {
+            obj.on_event(Event::Clicked, |context| {
+                //println!("Clicked on: home");
+            })
+            .set_grid_cell(
+                GridAlign::Stretch,
+                1,
+                1,
+                GridAlign::Stretch,
+                1,
+                1,
+            );
+
+            let mut btn_lbl = Label::new(obj);
+            btn_lbl.set_text(CString::new("HOME").unwrap().as_c_str());
+            btn_lbl.align_to(obj, Align::Center, 0, 0);
         });
 
-        let btn_move_zero = Btn::new(screen).apply(|obj| {
-            Label::new(obj)
-                .set_text(&CStr::from_bytes_with_nul(b"Move to Z=0\0").unwrap());
-            obj
-            .align_to(&position_label, Align::OutBottomMid, 0, spacing)
-            .add_flag(Flag::CHECKABLE)
-            .on_event(Event::Clicked, |context| {
-                let checked = context.btn_move_zero.has_state(State::CHECKED);
-                if checked {
-                    context.task_runner.enqueue_task(Task::MoveZero).unwrap();
-                } else {
-                    context.task_runner.cancel_task();
-                }
-            });
+        let btn_down = Btn::new(screen).apply(|obj| {
+            obj.on_event(Event::Clicked, |context| {
+                //println!("Clicked on: down");
+            })
+            .set_grid_cell(
+                GridAlign::Stretch,
+                2,
+                1,
+                GridAlign::Stretch,
+                1,
+                1,
+            );
+
+            let mut btn_lbl = Label::new(obj);
+            btn_lbl.set_text(CString::new("DOWN").unwrap().as_c_str());
+            btn_lbl.align_to(obj, Align::Center, 0, 0);
         });
 
-        Label::new(screen).apply(|obj| { obj
-            .set_text(&CStr::from_bytes_with_nul(b"Turbo Resin v0.1.3\0").unwrap())
-            .align_to(screen, Align::BottomRight, -5, -5);
+        let btn_stop = Btn::new(screen).apply(|obj| {
+            obj.on_event(Event::Clicked, |context| {
+                //println!("Clicked on: stop");
+            })
+            .set_grid_cell(
+                GridAlign::Stretch,
+                0,
+                3,
+                GridAlign::Stretch,
+                2,
+                1,
+            );
+
+            let mut btn_lbl = Label::new(obj);
+            btn_lbl.set_text(CString::new("STOP").unwrap().as_c_str());
+            btn_lbl.align_to(obj, Align::Center, 0, 0);
+        });
+
+        let mut current_pos = Label::new(screen).apply(|obj| {
+            obj.set_text(CString::new("0.0").unwrap().as_c_str());
+            obj.set_grid_cell(
+                GridAlign::Center,
+                0,
+                3,
+                GridAlign::Center,
+                3,
+                1,
+            );
         });
 
         Self {
-            btn_move_up, btn_move_down, btn_move_zero,
-            speed_label, position_label, speed_slider,
-            task_runner, zaxis,
+            style,
+            col_dsc,
+            row_dsc,
+            btn_0_1mm,
+            btn_1mm,
+            btn_10mm,
+            btn_up,
+            btn_home,
+            btn_down,
+            current_pos,
+            task_runner,
+            zaxis,
         }
     }
-
-    // Called before every frame rendering.
-    pub fn refresh(&mut self) {
-        // TODO check if the state has changed, this is burning a lot of CPU.
-        let c = self.task_runner.is_task_cancelled();
-        // We could use get/set state instead?
-        match self.task_runner.get_current_task() {
-            Some(Task::MoveUp) => {
-                if c { self.btn_move_up.add_state(State::DISABLED); }
-                self.btn_move_down.add_state(State::DISABLED);
-                self.btn_move_zero.add_state(State::DISABLED);
-            },
-            Some(Task::MoveDown) => {
-                self.btn_move_up.add_state(State::DISABLED);
-                if c { self.btn_move_down.add_state(State::DISABLED); }
-                self.btn_move_zero.add_state(State::DISABLED);
-            },
-            Some(Task::MoveZero) => {
-                self.btn_move_up.add_state(State::DISABLED);
-                self.btn_move_down.add_state(State::DISABLED);
-                if c { self.btn_move_zero.add_state(State::DISABLED); }
-            },
-            None => {
-                self.btn_move_up.clear_state(State::CHECKED | State::DISABLED);
-                self.btn_move_down.clear_state(State::CHECKED | State::DISABLED);
-                self.btn_move_zero.clear_state(State::CHECKED | State::DISABLED);
-            }
-        }
-
-        // set_text() makes a copy of the string internally.
-        self.position_label.set_text(&CStr::from_bytes_with_nul(
-            format!("Position: {:.2} mm\0", self.zaxis.get_current_position().as_mm()).as_bytes()
-        ).unwrap());
-
-        self.speed_label.set_text(&CStr::from_bytes_with_nul(
-            format!("Max speed: {:.2} mm/s\0", self.zaxis.get_max_speed().as_mm()).as_bytes()
-        ).unwrap());
-    }
+    pub fn refresh(&mut self) {}
 }
 
 #[derive(Debug, Clone, Copy)]
---
+++ src/ui/move_z_org.rs
@@ -0,0 +1,205 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use futures::Future;
+use lvgl::{
+    style::State,
+    core::Screen,
+    widgets::*,
+    prelude::*,
+};
+use alloc::format;
+
+use lvgl::cstr_core::CStr;
+use crate::{
+    TaskRunner,
+    drivers::zaxis::{
+        self,
+        prelude::*,
+    }, util::CancellableTask,
+};
+use crate::consts::zaxis::motion_control::*;
+
+pub struct MoveZ {
+    btn_move_up: Btn<Self>,
+    btn_move_down: Btn<Self>,
+    speed_slider: Slider<Self>,
+    speed_label: Label<Self>,
+    position_label: Label<Self>,
+    btn_move_zero: Btn<Self>,
+
+    task_runner: &'static TaskRunner<Task>,
+    zaxis: &'static zaxis::MotionControlAsync,
+}
+
+impl MoveZ {
+    pub fn new(
+        screen: &mut Screen::<Self>,
+        task_runner: &'static mut TaskRunner<Task>,
+        zaxis: &'static zaxis::MotionControlAsync,
+    ) -> Self {
+        use lvgl::widgets::*;
+        use lvgl::style::*;
+        use lvgl::core::*;
+
+        let spacing = 12;
+
+        let btn_move_up = Btn::new(screen).apply(|obj| {
+            Label::new(obj)
+                .set_text(&CStr::from_bytes_with_nul(b"Move Up\0").unwrap());
+            obj
+            .align_to(screen, Align::TopMid, 0, 2*spacing)
+            .add_flag(Flag::CHECKABLE)
+            .on_event(Event::Clicked, |context| {
+                let checked = context.btn_move_up.has_state(State::CHECKED);
+                if checked {
+                    context.task_runner.enqueue_task(Task::MoveUp).unwrap();
+                } else {
+                    context.task_runner.cancel_task();
+                }
+            });
+        });
+
+        let btn_move_down = Btn::new(screen).apply(|obj| {
+            Label::new(obj)
+                .set_text(&CStr::from_bytes_with_nul(b"Move Down\0").unwrap());
+            obj
+            .align_to(&btn_move_up, Align::OutBottomMid, 0, spacing)
+            .add_flag(Flag::CHECKABLE)
+            .on_event(Event::Clicked, |context| {
+                let checked = context.btn_move_down.has_state(State::CHECKED);
+                if checked {
+                    context.task_runner.enqueue_task(Task::MoveDown).unwrap();
+                } else {
+                    context.task_runner.cancel_task();
+                }
+            });
+        });
+
+        let speed_slider = Slider::new(screen).apply(|obj| { obj
+            .align_to(&btn_move_down, Align::OutBottomMid, 0, 2*spacing)
+            .set_range(1500, 10_000)
+            .set_value(10_000, 0)
+            .on_event(Event::ValueChanged, |context| {
+                let value = unsafe { lvgl::sys::lv_slider_get_value(context.speed_slider.raw) };
+
+                let value = (value as f32)/10000.0;
+                let value = value*value*value;
+                let value = value * MAX_SPEED;
+
+                context.zaxis.set_max_speed(value.mm());
+            });
+        });
+
+        let speed_label = Label::new(screen).apply(|obj| { obj
+            .align_to(&speed_slider, Align::OutBottomLeft, 50, spacing);
+        });
+
+        let position_label = Label::new(screen).apply(|obj| { obj
+            .align_to(&speed_label, Align::OutBottomLeft, 0, 0);
+        });
+
+        let btn_move_zero = Btn::new(screen).apply(|obj| {
+            Label::new(obj)
+                .set_text(&CStr::from_bytes_with_nul(b"Move to Z=0\0").unwrap());
+            obj
+            .align_to(&position_label, Align::OutBottomMid, 0, spacing)
+            .add_flag(Flag::CHECKABLE)
+            .on_event(Event::Clicked, |context| {
+                let checked = context.btn_move_zero.has_state(State::CHECKED);
+                if checked {
+                    context.task_runner.enqueue_task(Task::MoveZero).unwrap();
+                } else {
+                    context.task_runner.cancel_task();
+                }
+            });
+        });
+
+        Label::new(screen).apply(|obj| { obj
+            .set_text(&CStr::from_bytes_with_nul(b"Turbo Resin v0.1.3\0").unwrap())
+            .align_to(screen, Align::BottomRight, -5, -5);
+        });
+
+        Self {
+            btn_move_up, btn_move_down, btn_move_zero,
+            speed_label, position_label, speed_slider,
+            task_runner, zaxis,
+        }
+    }
+
+    // Called before every frame rendering.
+    pub fn refresh(&mut self) {
+        // TODO check if the state has changed, this is burning a lot of CPU.
+        let c = self.task_runner.is_task_cancelled();
+        // We could use get/set state instead?
+        match self.task_runner.get_current_task() {
+            Some(Task::MoveUp) => {
+                if c { self.btn_move_up.add_state(State::DISABLED); }
+                self.btn_move_down.add_state(State::DISABLED);
+                self.btn_move_zero.add_state(State::DISABLED);
+            },
+            Some(Task::MoveDown) => {
+                self.btn_move_up.add_state(State::DISABLED);
+                if c { self.btn_move_down.add_state(State::DISABLED); }
+                self.btn_move_zero.add_state(State::DISABLED);
+            },
+            Some(Task::MoveZero) => {
+                self.btn_move_up.add_state(State::DISABLED);
+                self.btn_move_down.add_state(State::DISABLED);
+                if c { self.btn_move_zero.add_state(State::DISABLED); }
+            },
+            None => {
+                self.btn_move_up.clear_state(State::CHECKED | State::DISABLED);
+                self.btn_move_down.clear_state(State::CHECKED | State::DISABLED);
+                self.btn_move_zero.clear_state(State::CHECKED | State::DISABLED);
+            }
+        }
+
+        // set_text() makes a copy of the string internally.
+        self.position_label.set_text(&CStr::from_bytes_with_nul(
+            format!("Position: {:.2} mm\0", self.zaxis.get_current_position().as_mm()).as_bytes()
+        ).unwrap());
+
+        self.speed_label.set_text(&CStr::from_bytes_with_nul(
+            format!("Max speed: {:.2} mm/s\0", self.zaxis.get_max_speed().as_mm()).as_bytes()
+        ).unwrap());
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Task {
+    MoveUp,
+    MoveDown,
+    MoveZero,
+}
+
+impl CancellableTask for Task {
+    type Context = zaxis::MotionControlAsync;
+
+    type RunFuture<'a> = impl Future<Output = ()> + 'a where Self: 'a;
+    type CancelFuture<'a> = impl Future<Output = ()> + 'a where Self: 'a;
+
+    fn run<'a>(&'a self, mc: &'a mut zaxis::MotionControlAsync) -> Self::RunFuture<'a> {
+        async move {
+            match self {
+                Self::MoveUp => mc.set_target_relative(40.0.mm()),
+                Self::MoveDown => mc.set_target_relative((-40.0).mm()),
+                Self::MoveZero => {
+                    let s = mc.get_max_speed();
+                    zaxis::calibrate_origin(mc, None).await;
+                    // FIXME we don't restore the original speed when the task is cancelled.
+                    mc.set_max_speed(s);
+                    mc.set_target(0.0.mm());
+                }
+            };
+            mc.wait(zaxis::Event::Idle).await;
+        }
+    }
+
+    fn cancel<'a>(&'a self, mc: &'a mut zaxis::MotionControlAsync) -> Self::CancelFuture<'a> {
+        async move {
+            // The task was cancelled
+            mc.stop();
+            mc.wait(zaxis::Event::Idle).await;
+        }
+    }
+}
